3a5ef2eb0b3864e5aafa56979e59d0b2d69d5612
xxmadisons <madstanley00@gmail.com>
2022-03-14 23:02:41 -0600

    M10: 07-02

--- Startup.cs
+++ Startup.cs
@@ -45,6 +45,10 @@ namespace Bookstore
 
             services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddServerSideBlazor();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,6 +88,10 @@ namespace Bookstore
                 endpoints.MapDefaultControllerRoute();
 
                 endpoints.MapRazorPages();
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
+
             });
         }
     }
