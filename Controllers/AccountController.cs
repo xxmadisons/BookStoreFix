bf3039f03ea4fedd22711d8c7817de769d12bb2c
xxmadisons <madstanley00@gmail.com>
2022-03-16 11:45:18 -0600

    M11: 09-06

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> um, SignInManager<IdentityUser> sim)
+        {
+            userManager = um;
+            signInManager = sim;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Username);
+
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+
+                    if ((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin");
+                    }
+
+                    
+                }
+
+            }
+            ModelState.AddModelError("", "Invalid username or password.");
+            return View(loginModel);
+
+
+
+        }
+
+
+
+    }
+}
